c5bad1eca87eea8a7c5853d34080dceebb513383
yrtravi <yrt2492@gmail.com>
2021-02-18 07:57:29 +0530

    'description'

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { UserForm } from "./components/UserForm";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <UserForm />
+      </div>
+    );
+  }
+}
+
+export default App;
